e419c19ca6b883cb1ecf3ca98b2263dda5c680d4
vinit <96609006+21bca@users.noreply.github.com>
2022-01-05 10:47:25 +0530

    solution 4

---
+++ Solutions/SUMDIGIT.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,r,sum=0;
+	clrscr();
+	printf("\n Enetr any number:");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		r=n%10;
+		sum=sum+r;
+		n=n/10;
+	}
+	printf("\n sum of digit  is: %d",sum);
+	getch();
+}
